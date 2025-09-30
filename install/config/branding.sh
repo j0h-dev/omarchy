6d88cbe0ddbcc0bae800c528efd43e009c10e951
Jaro Heinonen <heinonenjaro@proton.me>
2025-10-01 01:08:05 +0300

    feat: remove screensaver

--- install/config/branding.sh
+++ install/config/branding.sh
@@ -1,4 +1,3 @@
-# Allow the user to change the branding for fastfetch and screensaver
+# Allow the user to change the branding for fastfetch
 mkdir -p ~/.config/omarchy/branding
 cp ~/.local/share/omarchy/icon.txt ~/.config/omarchy/branding/about.txt
-cp ~/.local/share/omarchy/logo.txt ~/.config/omarchy/branding/screensaver.txt
