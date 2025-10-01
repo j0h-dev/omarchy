f55555644b3e210dfb8d572e9cc8a847681b2fd6
Jaro Heinonen <heinonenjaro@proton.me>
2025-10-01 23:52:00 +0300

    feat: setup zsh config

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,6 @@ cp -R ~/.local/share/omarchy/config/* ~/.config/
 
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
+
+# Use default zshrc from Omarchy
+cp ~/.local/share/omarchy/default/zshrc ~/.zshrc
